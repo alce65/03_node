46f3c6446294e26a3c32884981a8e49819be4591
Alejandro Cerezo <alce65@hotmail.es>
2022-02-23 15:57:52 +0100

    Update calculator

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 import { greetings, args, calculator } from './app.js';
+import inquirer from 'inquirer';
 
 greetings();
 console.log(process.argv);
-console.log(args(process.argv));
+const calcArgs = args(process.argv);
+console.log();
 
-try {
-    console.log(calculator(args(process.argv)));
-} catch (error) {
-    console.log(error.message);
+if (calcArgs.length < 2) {
+    inquirer
+        .prompt([
+            { name: 'arg1', message: 'Primer valor?', type: 'number' },
+            { name: 'arg2', message: 'Segundo valor?', type: 'number' },
+        ])
+        .then((resp) => {
+            try {
+                console.log(calculator([resp.arg1, resp.arg2]));
+            } catch (error) {
+                console.log(error.message);
+            }
+        });
+} else {
+    try {
+        console.log(calculator(calcArgs));
+    } catch (error) {
+        console.log(error.message);
+    }
 }
